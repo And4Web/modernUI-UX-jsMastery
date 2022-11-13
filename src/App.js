859b0cc0ce7dac1f16e6f01e1a9d77320327d1b0
And4Web <anands2807@gmail.com>
2022-11-13 19:58:28 +0530

    started the project and created components and container folders

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import './App.css'
+import {Brand, CTA, Navbar} from './components';
+import {Footer, Features, Blog, Possibility, WhatGPT3, Header} from './containers'
+
 function App() {
   return (
-    <div >
-      <h1>Hello</h1>
+    <div className='App'>
+      <div className="gradient__bg">
+        <Navbar/>
+        <Header/>
+      </div>
+      <Brand/>
+      <WhatGPT3/>
+      <Features/>
+      <Possibility/>
+      <CTA/>
+      <Blog/>
+      <Footer/>
     </div>
   );
 }
 
 export default App;
+
+
+// 28 minutes
